c788d6defa8cb5a8ff8f67375755b78147bc5c57
jpblicer <jpblicer@gmail.com>
2022-01-23 10:08:09 +0900

    Fixed computerSelection with array. Prompt player for choice.

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-let randomNum= Math.floor(Math.random() * 3)
+
 let computerSelection = computerPlay;
 
-function computerPlay(randomNum){
-    if(randomNum===0){
-        return "rock";
-    }
-    else if(randomNum===1){
-        return "paper";
-    }
-    else if(randomNum===2){
-        return "scissors";
-    }
-}
-// wtf how to get computer play and selection to work
-    
+
+function computerPlay() { 
+    const choices = ['rock', 'paper', 'scissors'] 
+    return choices[Math.floor(Math.random() * choices.length)];
+} 
+
 
 let playerScore = 0;
 let computerScore = 0;
 
-let playerSelection = "rock".toLowerCase();
-
+//let playerSelection = "rock".toLowerCase();
+let playerSelection = prompt("Will you choose Rock, Paper or Scissors?");
+if (playerSelection) {
+    let playerPick = playerSelection.toLowerCase();
+    if (playerPick === "rock" || playerPick === "paper" || playerPick === "scissors"){}
+}   else {
+    alert("invalid selection noob. Git Gud");
+}
 
 
 
@@ -56,90 +55,26 @@ function playRound(playerSelection, computerSelection){
     }
  }
     
-console.log(computerSelection(randomNum))
-console.log(playRound(playerSelection, computerSelection))
 
-/*
-
-
-
-let playerScore = parseInt(0);
-let computerScore = parseInt(0);
+console.log(playRound(playerSelection, computerPlay()))
+console.log(playRound(playerSelection, computerPlay()))
+console.log(playRound(playerSelection, computerPlay()))
+console.log(playRound(playerSelection, computerPlay()))
+console.log(playRound(playerSelection, computerPlay()))
+console.log(playerScore, computerScore)
 
 
-function computerPlay(randomNum){
-    if(randomNum===0){
-        return "rock";
+
+function result(playerScore, computerScore){
+    if(playerScore > computerScore){
+        return "You win!";
     }
-    else if(randomNum===1){
-        return "paper";
-    }
-
-    else if(randomNum===2){
-        return "scissors";
-    }
-    else{
-        return "Nothing Selected"
-    }
-    
-}
-
-
-
-
-
-function playRound(playerSelection, computerSelection){
-    if (playerSelection === computerSelection){
-        computerScore++;
-        playerScore++;
-        return "Tie"
-    }
-    else if(playerSelection==="rock" && computerSelection==="paper"){
-        computerScore++;
-        return "Computer picked Paper. You Lose."
-    }
-    else if(playerSelection==="rock" && computerSelection==="scissors"){
-        playerScore++;
-        return "Computer picked Scissors. You Win!"
-    }    
-    else if(playerSelection==="paper" && computerSelection==="rock"){
-        playerScore++;
-        return "Computer picked Rock. You Win!"
-    }
-    else if(playerSelection==="paper" && computerSelection==="scissors"){
-        computerScore++;
-        return "Computer picked Scissors. You Lose."
-    }  
-    else if(playerSelection==="scissors" && computerSelection==="rock"){
-        computerScore++;
-        return "Computer picked Rock. You Lose."
-    }  
-    else if(playerSelection==="scissors" && computerSelection==="paper"){
-        playerScore++;
-        return "Computer picked Paper. You Win!"
-    } 
-}
-
-function gameResult(playerScore, computerScore){
-    if (playerScore > computerScore)
-    return "You win with " ,playerScore, " Points!";
-    else if (playerScore < computerScore){
-    return "You Lost. Computer Got " ,computerScore, " Points!";
+    if(playerScore < computerScore){
+        return "Computer wins!"
     }
     else {
-        return "It is a Tie!"
+        return "It is a tie!"
     }
 }
 
-console.log(playRound("rock".toLowerCase(), computerSelection))
-console.log(playRound("paper".toLowerCase(), computerSelection))
-console.log(playRound("scissors".toLowerCase(), computerSelection))
-console.log(playRound("scissors".toLowerCase(), computerSelection))
-console.log(playRound("rock".toLowerCase(), computerSelection))
-
-console.log(playerScore)
-console.log(computerScore)
-console.log(gameResult(playRound))
-
-
-*/
+console.log(result(playerScore, computerScore))
